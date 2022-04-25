174a08154501e7b3867f37f0ed34069692f93a24
Mysiak <Patrycja.Mysiak@PerkinElmer.com>
2022-04-25 22:36:01 +0200

    form added, styles updated

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,10 +1,25 @@
 import React from "react"
 
-
 export default function Add() {
     return(
         <div className="add">
             <h4 className="section-title">Add new transaction</h4>
+            <div className="form">
+                <label for="enterText">Text
+                </label><br></br>
+                <input 
+                    type="text"
+                    placeholder="Enter text..."
+                    name="enterText"
+                /><br></br>
+                <label for="amount">Amount <br></br> Negative - expense Positive - income</label><br></br>
+                <input
+                    type="text"
+                    placeholder="Enter amount.."
+                    name="amount"
+                />
+                <button type="submit">Add transaction</button>
+            </div>
         </div>
     )
 }
